72436040061ce45334b44c120c6253be56d64470
c0de-dLax <ojtc.ops@gmail.com>
2024-04-03 02:32:08 +0800

    Change keyName of getItem() method from 'theme' to 'mode'

--- src/components/ThemeSwitch.tsx
+++ src/components/ThemeSwitch.tsx
@@ -3,37 +3,37 @@ import React, { useState, useEffect } from "react";
 import { FaMoon, FaSun } from "react-icons/fa6";
 
 const ThemeSwitch: React.FC = () => {
-  const initialThemeMode = localStorage.getItem("theme") || "light";
-  const [themeMode, setThemeMode] = useState(initialThemeMode);
+  const initialTheme = localStorage.getItem("mode") || "light";
+  const [theme, setTheme] = useState(initialTheme);
 
-  const selectThemeHandler = (newThemeMode: string) => {
-    if (newThemeMode === "dark" || newThemeMode === "light") {
-      setThemeMode(newThemeMode);
-      localStorage.setItem("theme", newThemeMode);
+  const selectThemeHandler = (newTheme: string) => {
+    if (newTheme === "dark" || newTheme === "light") {
+      setTheme(newTheme);
+      localStorage.setItem("mode", newTheme);
     }
   };
 
   useEffect(() => {
-    if (themeMode === "dark") {
+    if (theme === "dark") {
       document.body.classList.add("dark");
     } else {
       document.body.classList.remove("dark");
     }
-  }, [themeMode]);
+  }, [theme]);
 
   return (
     <section className="inline-block float-left">
-      <label className="switch" title={themeMode === "dark" ? "Click to switch to Light theme" : "Click to switch to Dark theme"}>
+      <label className="switch" title={theme === "dark" ? "Click to switch to Light theme" : "Click to switch to Dark theme"}>
         <input
           name="Theme switch"
           
           type="checkbox"
           onClick={() =>
-            selectThemeHandler(themeMode === "dark" ? "light" : "dark")
+            selectThemeHandler(theme === "dark" ? "light" : "dark")
           }
         />
-        <span className={`slider round ${themeMode === "dark" ? "dark" : ""}`}>
-          {themeMode === "dark" ? (
+        <span className={`slider round ${theme === "dark" ? "dark" : ""}`}>
+          {theme === "dark" ? (
             <div className="mt-[1px] mr-7 py-1 px-[5px] cursor-pointer rounded-[8px] select-none">
                <FaSun
                 aria-label="Switch to dark theme"
